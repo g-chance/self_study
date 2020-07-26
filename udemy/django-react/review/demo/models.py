31ee3755cd3f674fe31f3c63637e11d44f7e1646
Greg Chance <ge.chance@gmail.com>
2020-07-25 17:08:55 -0700

    django-react course

---
+++ udemy/django-react/review/demo/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=36, blank=False, unique=True)
+    desc = models.TextField(max_length=256, blank=True)
+    price = models.IntegerField(default=0)
+    published = models.DateTimeField(blank=True, null=True, default=None)
+    is_published = models.BooleanField(default=False)
+    cover = models.FileField(upload_to='covers/', blank=True)
