8e2356c076c22bafbb0fa592765b994f4d0fad72
Greg Chance <ge.chance@gmail.com>
2020-08-04 15:39:18 -0700

    updates

--- udemy/django-react/movierater/api/models.py
+++ udemy/django-react/movierater/api/models.py
@@ -17,11 +17,11 @@ class Movie(models.Model):
         return len(ratings)
 
     def avg_rating(self):
-        sum = 0
+        su = 0
         ratings = Rating.objects.filter(movie=self)
         for rating in ratings:
-            sum += rating.stars
-        return (sum / len(ratings)) if len(ratings) > 0 else 0
+            su += rating.stars
+        return (su / len(ratings)) if len(ratings) > 0 else 0
 
 class Rating(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
