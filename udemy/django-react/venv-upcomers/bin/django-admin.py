7bd9c254fa54e5ceb506aabd5f1b6de7a5cdfbc3
Greg Chance <ge.chance@gmail.com>
2020-08-10 22:03:56 -0700

    updates

--- udemy/django-react/venv-upcomers/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/gchance/Documents/webDev/self_study/udemy/django-react/venv-upcomers/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
