0f22ed562fd48815290985e39a4968b96565c050
Greg Chance <ge.chance@gmail.com>
2020-10-25 11:46:30 -0700

    add congrats and testutils

--- udemy/testing-jest-enzyme/jotto/src/Congrats.js
+++ udemy/testing-jest-enzyme/jotto/src/Congrats.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
 
-const Congrats = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
-
-export default Congrats
+export default (props) => {
+    if (props.success) {
+        return (
+            <div data-test="component-congrats">
+                <span data-test="congrats-message">
+                    Congrations! You guessed the word!
+                    </span>
+            </div>
+        );
+    } else {
+        return (
+            <div data-test="component-congrats" />
+        );
+    }
+};
