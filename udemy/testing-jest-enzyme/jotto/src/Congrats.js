6d295215f9d39e76554110bd85e0589e8d4db6ec
Greg Chance <ge.chance@gmail.com>
2020-10-26 21:29:36 -0700

    stuff

--- udemy/testing-jest-enzyme/jotto/src/Congrats.js
+++ udemy/testing-jest-enzyme/jotto/src/Congrats.js
@@ -2,23 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Congrats = (props) => {
-    if (props.success) {
-        return (
-            <div data-test="component-congrats">
-                <span data-test="congrats-message">
-                    Congrations! You guessed the word!
+   if (props.success) {
+      return (
+         <div data-test="component-congrats" className="alert alert-success">
+            <span data-test="congrats-message">
+               Congrations! You guessed the word!
                     </span>
-            </div>
-        );
-    } else {
-        return (
-            <div data-test="component-congrats" />
-        );
-    }
+         </div>
+      );
+   } else {
+      return (
+         <div data-test="component-congrats" />
+      );
+   }
 };
 
 Congrats.propTypes = {
-    success: PropTypes.bool.isRequired,
-}
+   success: PropTypes.bool.isRequired,
+};
 
 export default Congrats;
