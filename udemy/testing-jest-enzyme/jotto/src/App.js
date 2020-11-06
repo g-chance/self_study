afe172890a31e0d3f6ce1c38fefc39bb9ce803b1
Greg Chance <ge.chance@gmail.com>
2020-11-06 11:44:33 -0800

    setup input component and storeFactory

--- udemy/testing-jest-enzyme/jotto/src/App.js
+++ udemy/testing-jest-enzyme/jotto/src/App.js
@@ -5,15 +5,15 @@ import GuessedWords from './GuessedWords';
 import Congrats from './Congrats';
 
 function App() {
-   return (
-      <div className="container">
-         <h1>Jotto</h1>
-         <Congrats success={ true } />
-         <GuessedWords guessedWords={[
-            {guessedWord: 'train', letterMatchCount: 3}
-         ]} />
-      </div>
-   );
+    return (
+        <div className="container">
+            <h1>Jotto</h1>
+            <Congrats success={true} />
+            <GuessedWords guessedWords={[
+                { guessedWord: 'train', letterMatchCount: 3 }
+            ]} />
+        </div>
+    );
 }
 
 export default App;
