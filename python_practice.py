631f590c003e1d5c8022d27a2750487d36266db6
Greg Chance <ge.chance@gmail.com>
2020-11-15 22:16:55 -0800

    python practice

--- python_practice.py
+++ python_practice.py
@@ -1,4 +1,20 @@
+#   ==================== Binary Search Review ====================
 
+# def checkForNum(arr, num):
+
+#     lo, hi = 0, len(arr)-1
+    
+#     while lo <= hi:
+#         mid = (lo + hi) // 2
+#         if num == arr[mid]:
+#             return True
+#         elif num > arr[mid]:
+#             lo = mid + 1
+#         else:
+#             hi = mid - 1
+#     return False
+
+# print(checkForNum([1,2,3,4,5], 2.5))
 
 #   ==================== Number Conversion ====================
 
